555d172bbee0e6c5101e77dd93defc53fc67049b
Michael Ato Hutchful <ato.hutchful@gmail.com>
2017-07-19 09:59:12 +0000

    Add project files.

---
+++ BotPostSample/Controllers/SampleController.cs
@@ -0,0 +1,19 @@
+﻿using System.Net;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace BotPostSample.Controllers
+{
+    public class SampleController : ApiController
+    {
+        public IHttpActionResult PostAsync([FromBody]SampleData SampleString)
+        {
+            return Ok(HttpStatusCode.OK);
+        }
+
+        public class SampleData
+        {
+            public string testString { get; set; }
+        }
+    }
+}
